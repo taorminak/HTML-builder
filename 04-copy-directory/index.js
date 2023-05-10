e77c58046f9af1a24cb3706b152c1bf2e40daa02
taorminak <ostanina.anna@yahoo.com>
2023-05-10 15:06:28 +0200

    fix: change code in 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,15 +3,23 @@ const path = require('path');
 
 (async () => {
   try {
-  
+    const filesCopyDir = path.join(__dirname, 'files-copy');
+
     await fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
   
     const files = await fs.readdir(path.join(__dirname,'files'));
-  
+    const filesCopy = await fs.readdir(filesCopyDir);
+
+    for (const fileCopy of filesCopy) {
+      if (!files.includes(fileCopy)) {
+        await fs.unlink(path.join(filesCopyDir, fileCopy));
+      }
+    }
+
     for (const file of files) {
       await fs.copyFile(
         path.join(__dirname, 'files', file),
-        path.join(__dirname, 'files-copy', file)
+        path.join(filesCopyDir, file)
       );
       console.log(`${file} был скопирован`);
     }
